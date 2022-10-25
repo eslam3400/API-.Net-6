c299015d809c5bd7bc28a08a1a782ffffb4222de
eslam3400 <eslam.magdy@twerlo.com>
2022-10-25 13:37:23 +0200

    Add project files.

---
+++ Super Hero/Controllers/SuperHeroController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Super_Hero.Models;
+
+namespace Super_Hero.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SuperHeroController : Controller
+    {
+        private readonly DataContext context;
+
+        public SuperHeroController(DataContext context) { this.context = context; }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAll() { 
+            return Ok(await this.context.SuperHeroes.ToListAsync());
+        }
+        
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<SuperHero>> GetOne(int id) {
+            var hero = await context.SuperHeroes.FindAsync(id);
+            if (hero == null) return BadRequest("No Hero Found!");
+            return Ok(hero); 
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<SuperHero>> Add(SuperHero hero){
+            await context.SuperHeroes.AddAsync(hero);
+            await context.SaveChangesAsync();
+            return Ok(hero);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<ActionResult<SuperHero>> Update(int id, SuperHero hero){
+            var _hero = await context.SuperHeroes.FindAsync(id);
+            if(_hero == null) return NotFound("No Hero Founded!");
+            _hero.Name = hero.Name;
+            _hero.FirstName = hero.FirstName;
+            _hero.LastName = hero.LastName;
+            _hero.Place = hero.Place;
+            await context.SaveChangesAsync();
+            return Ok(_hero);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<ActionResult<SuperHero>> Delete(int id)
+        {
+            var hero = await context.SuperHeroes.FindAsync(id);
+            if (hero == null) return NotFound("No Hero Founded!");
+            context.SuperHeroes.Remove(hero);
+            await context.SaveChangesAsync();
+            return Ok(hero);
+        }
+    }
+}
---
+++ Super Hero/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Super_Hero.Models;
+
+namespace Super_Hero
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {  }
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
